46dd18da13c070283e91c12b052056e4f884d16a
ali bavarsad <a.bavarsad007@gmail.com>
2022-01-20 23:26:20 +0000

    feat: UG1 add Dashboard fixes #4

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,39 @@
+import React, { useEffect, useState } from "react";
+import PlanCard from "../components/PlanCard";
+import Grid from "@mui/material/Grid";
+import { Container } from "@mui/material";
 
-export default function Dashboard (){
-    return <>
-     </>;
+function Dashboard() {
+  const [plans, setPlans] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/plans")
+      .then((res) => res.json())
+      .then((data) => setPlans(data));
+  }, []);
+
+  //this is a function to delete plans from database
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/plans/" + id, {
+      method: "DELETE",
+    });
+
+    //this is a function to update  plans in browser when delete a plan
+    const newPlans = plans.filter((plan) => plan.id !== id);
+    setPlans(newPlans);
+  };
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        {plans.map((plan) => (
+          <Grid item key={plan.id} xs={12} md={6} lg={4}>
+            <PlanCard plan={plan} handleDelete={handleDelete} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
+
+export default Dashboard;
