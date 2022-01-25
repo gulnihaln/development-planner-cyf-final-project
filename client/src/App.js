97a2818c40fa5341e23be8c49e9cc0b7c9890881
Mor Khojasteh <mori.kh1987@gmail.com>
2022-01-25 14:26:00 +0000

    fix: authentication of login and add redirection to dashboard fixes #58

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import { Route, Switch } from "react-router-dom";
-import React, { useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
 import NewPlan from "./pages/NewPlan";
 import Plan from "./pages/Plan";
+import { verifyUser } from "./utils/api";
 
 const App = () => {
-	const [authorised, setAuthorised] = useState(false);
-	console.log(authorised);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	console.log(isAuthenticated);
+
+	useEffect(() => {
+		verifyUser();
+	}, []);
+
+
 	return (
 		<div>
 			<Header />
 			<hr />
 			<Switch>
-				<Route path="/login" exact>
-					<Login setAuthorised={setAuthorised} />
-				</Route>
+				<Route
+					exact
+					path="/"
+					render={(props) =>
+						!isAuthenticated ? (
+							<Login {...props} setIsAuthenticated={setIsAuthenticated} />
+						) : (
+							<Redirect to="/dashboard" />
+						)
+					}
+				/>
 				<Route path="/dashboard" exact>
 					<Dashboard />
 				</Route>
