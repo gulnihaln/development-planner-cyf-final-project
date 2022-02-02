fdbe39b8815cf3d572b71d686fe0a42b61725664
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-02 00:13:11 +0000

    fix: add props to editabletitle in goals and change style of add new goal button

--- client/src/components/AddGoalButton.js
+++ client/src/components/AddGoalButton.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-export default function AddGoalButton ({ newGoalHandle }) {
+export default function AddGoalButton ({ HandleNewGoal }) {
 	return (
 		<div>
 			<Button
 				// className="new-goal-button"
-				onClick={newGoalHandle}
+				onClick={HandleNewGoal}
 				variant="outlined"
 				sx={{
 					color: "primary.main",
 					bgColor: "#FFFFFF",
-					borderRadius: 5,
+					borderRadius: "4px",
 					marginTop: 3,
-					width: 350,
+					width: 300,
 					marginLeft: 4,
 					alignSelf: "center",
 				}}
