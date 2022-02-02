9d743098463a02fe9579b084e66ed0959f0c140a
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-02 22:08:46 +0000

    feat: add post request for goal and fixes new goal handle fixes #114

--- client/src/components/Goals.js
+++ client/src/components/Goals.js
@@ -6,29 +6,33 @@ import Container from "@mui/material/Container";
 import { request } from "../utils/api";
 
 export default function Goals({ goals, setGoals, plan_id }) {
+	console.log({goals});
 	useEffect(() => {
 		request.get(`/plans/${plan_id}/goals`).then((res) => {
 			setGoals(res.data);
 		});
 	}, [plan_id, setGoals]);
-
-	function HandleNewGoal() {
-		setGoals((prev) => {
-			return goals.concat({
-				title: "",
-				status:"",
-				startDate:"",
-				endDate:"",
-				tasks: [],
-			});
+	const postGoal = async (body) => {
+		const response = await request.post(`/plans/${plan_id}/goals`, body, {
+			headers: { "Content-Type": "application/json" },
 		});
-	}
+		return response;
+	};
+	function HandleNewGoal() {
+			const body = {
+				title: "Goal title",
+				status: "incomplete",
+				start_date: "2022-02-02",
+				end_date: "2022-02-02",
+			};
+			postGoal(body).then((response) =>  setGoals((prev)=>prev.concat(response.data)));
+		}
 	return (
 		<Container>
 			<Grid container spacing={3} sx={{ marginTop: 1 }}>
-				{goals.map((goal, index) => {
+				{goals.map((goal) => {
 					return (
-						<Grid key={index} item lg={4} md={6} xs={12}>
+						<Grid key={goal.goal_id} item lg={4} md={6} xs={12}>
 							<GoalCard
 								goal={goal}
 								goals={goals}
