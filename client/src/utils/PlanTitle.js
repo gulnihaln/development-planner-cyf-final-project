b3d3906a7ac0cf13cb2e54c1a9719f1f8aa7e697
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-08 18:11:36 +0000

    backend integration for editable plan title and plan description fixes #147, fixes #148

--- client/src/utils/PlanTitle.js
+++ client/src/utils/PlanTitle.js
@@ -1,33 +1,91 @@
-import { Typography, InputBase } from "@mui/material";
 import React, { useState } from "react";
+import { Box, Typography, TextField } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 
-export default function PlanTitle({ title, setTitle }) {
-    const [open, setOpen] = useState(false);
+const CssTextField = styled(TextField, {
+	shouldForwardProp: (props) => props !== "focusColor",
+})((p) => ({
+	"& .MuiInput-underline:after": {
+		borderBottom: "1px solid",
+		borderBottomColor: p.focusColor,
+	},
+}));
 
-    return (
-			<div>
-				{open ? (
-					<div>
-						<InputBase
-							value={title}
-							onChange={(event) => setTitle(event.target.value)}
-							inputProps={{}}
-							fullWidth
-							onBlur={() => setOpen(!open)}
-						></InputBase>
-					</div>
-				) : (
-					<Box>
-						<Typography
-							onclick={setOpen(!open)}
-							sx={{ cursor: "pointer" }}>
-							{title}
-						</Typography>
-						<MoreVertIcon />
-					</Box>
-				)}
-			</div>
-		);
+export default function PlanTitle({
+	plan_id,
+	description,
+	title,
+	setTitle,
+	setDescription, editPlan,
+}) {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div>
+			{open ? (
+				<Box sx={{ fontSize: "20px", letterSpacing: "2px" }}>
+					<CssTextField
+						// label="Edit plan title"
+						hiddenLabel
+						variant="standard"
+						focusColor="rgba(0, 0, 0, 0.6)"
+						value={title}
+						onChange={(event) => setTitle(event.target.value)}
+						fullWidth
+						onBlur={() => {
+							editPlan(plan_id, title, description);
+							setOpen(false);
+						}}
+						InputProps={{ disableUnderline: true }}
+						InputLabelProps={{ style: { color: "rgba(0, 0, 0, 0.6)" } }}
+					/>
+				</Box>
+			) : (
+				<Box>
+					<Typography
+						variant="h5"
+						onclick={setOpen(true)}
+						sx={{ cursor: "pointer" }}
+					>
+						{title}
+					</Typography>
+					<MoreVertIcon />
+				</Box>
+			)}
+			{open ? (
+				<Box sx={{ fontStyle: "italic" }}>
+					<CssTextField
+						// label="Edit plan description"
+						hiddenLabel
+						variant="standard"
+						sx={{ borderColor: "transparent" }}
+						// focusColor="rgba(0, 0, 0, 0.6)"
+						value={description}
+						onChange={(event) => setDescription(event.target.value)}
+						fullWidth
+						onBlur={() => {
+							editPlan(plan_id, title, description);
+							setOpen(false);
+						}}
+						InputProps={{ disableUnderline: true }}
+						InputLabelProps={{ style: { color: "rgba(0, 0, 0, 0.6)" } }}
+					/>
+				</Box>
+			) : (
+				<Box>
+					<Typography
+						onclick={setOpen(true)}
+						sx={{
+							flexGrow: 1,
+							paddingLeft: 1,
+						}}
+					>
+						{description}
+					</Typography>
+					<MoreVertIcon />
+				</Box>
+			)}
+		</div>
+	);
 }
