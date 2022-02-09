e1fe1080db912871d8b2d9c8b1e2b8028e68c191
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-09 21:19:04 +0000

    feat: add checkbox for tasks and update status in db fixes #154, fixes #155

--- client/src/components/TaskTicket.js
+++ client/src/components/TaskTicket.js
@@ -5,26 +5,18 @@ import Card from "@mui/material/Card";
 import { request } from "../utils/api";
 import IconButton from "@mui/material/IconButton";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
-import { Tooltip, Checkbox } from "@mui/material";
+import { Tooltip } from "@mui/material";
 import EditableTask from "../utils/EditableTask";
+import TaskStatus from "./TaskStatus";
 
-const completed = { inputProps: {
-	"aria-label": "Completed Checkbox",
-	},
-};
-
-export default function TaskTicket ({ tasks, plan_id, goal, goal_id, setTasks }) {
-	// const [description, setDescription] = useState();
-	// const [taskStatus, setTaskStatus] = useState();
-	const [checked, setChecked] = React.useState(false);
-
+export default function TaskTicket({ tasks, plan_id, goal, setTasks, setTasksAndUpdateGoal }) {
 	const handleDeleteTask = async (task_id) => {
 		request.delete(`/plans/${plan_id}/goals/${goal.goal_id}/tasks/${task_id}`);
 		const newTasks = tasks.filter((task) => task.id !== task_id);
 		setTasks(newTasks);
 	};
-	const editTask = async (task_id, description, taskStatus) => {
-		const body = { description, status: taskStatus };
+	const editTask = async (task_id, description, status) => {
+		const body = { description, status };
 		const response = await request.put(
 			`/plans/${plan_id}/goals/${goal.goal_id}/tasks/${task_id}`,
 			body,
@@ -37,55 +29,50 @@ export default function TaskTicket ({ tasks, plan_id, goal, goal_id, setTasks })
 			const index = prev.findIndex((task) => task.id === task_id);
 			const newTasks = [...prev];
 			newTasks[index] = response.data;
+			setTasksAndUpdateGoal(newTasks);
 			return newTasks;
 		});
+		return response;
 	};
-
-	const handleChangeTaskStatus = (task_id, event) => {
-		setChecked(event.target.checked);
-		const status = checked ? "completed" : "uncompleted";
-		editTask(plan_id, goal_id, task_id, status)
-			.then((res) => {
-				console.log(res);
-			})
-			.catch((e) => console.log(e));
-	};
-
 
 	return (
 		<>
 			<Box>
-				{tasks.map((task) => {
-					return (
-						<Card
-							key={task.id}
-							sx={{ width: "100%", marginTop: 2, padding: 0 }}
-						>
-							<Checkbox
-								{...completed}
-								checked={checked}
-								onChange={(e) => {
-									handleChangeTaskStatus(task.task_id, e);
+				{tasks
+					.sort((a, b) => (a.id > b.id ? 1 : -1))
+					.map((task) => {
+						return (
+							<Card
+								key={task.id}
+								sx={{
+									display: "flex",
+									justifyContent: "space-between",
+									width: "100%",
+									marginTop: 2,
+									padding: 0,
 								}}
-							/>
-							<CardHeader
-								action={
-									<Tooltip title="Remove task">
-										<IconButton
-											onClick={() => handleDeleteTask(task.id)}
-											sx={{
-												color: "rgba(0, 0, 0, 0.54)",
-											}}
-										>
-											<ClearOutlinedIcon sx={{ fontSize: "medium" }} />
-										</IconButton>
-									</Tooltip>
-								}
-								subheader={<EditableTask task={task} editTask={editTask} />}
-							/>
-						</Card>
-					);
-				})}
+							>
+								<Box sx={{ display: "flex" }}>
+									<TaskStatus task={task} editTask={editTask} />
+									<CardHeader
+										sx={{ padding: 0 }}
+										subheader={<EditableTask task={task} editTask={editTask} />}
+									/>
+								</Box>
+								<Tooltip title="Remove task">
+									<IconButton
+										onClick={() => handleDeleteTask(task.id)}
+										sx={{
+											alignSelf: "flex-start",
+											color: "rgba(0, 0, 0, 0.54)",
+										}}
+									>
+										<ClearOutlinedIcon sx={{ fontSize: "medium" }} />
+									</IconButton>
+								</Tooltip>
+							</Card>
+						);
+					})}
 			</Box>
 		</>
 	);
