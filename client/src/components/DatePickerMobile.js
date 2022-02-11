1cf2a6260407448bc25d1e38713636889307ce18
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-11 15:12:32 +0000

    feat: add mobile datepicker fixes #173

--- client/src/components/DatePickerMobile.js
+++ client/src/components/DatePickerMobile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
@@ -6,29 +6,62 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import MobileDateRangePicker from "@mui/lab/MobileDateRangePicker";
 import "../styles/Goal.css";
+import { styled } from "@mui/material/styles";
 
-export default function DatePickerMobile () {
-    const [value, setValue] = React.useState([null, null]);
-    return (
-			<div>
-				<LocalizationProvider dateAdapter={AdapterDateFns}>
-					<Stack spacing={3} className="mobile-datepicker">
-						<MobileDateRangePicker
-							startText="Start"
-							value={value}
-							onChange={(newValue) => {
-								setValue(newValue);
-							}}
-							renderInput={(startProps, endProps) => (
-								<React.Fragment>
-									<TextField {...startProps} />
-									<Box sx={{ mx: 2 }}> to </Box>
-									<TextField {...endProps} />
-								</React.Fragment>
-							)}
-						/>
-					</Stack>
-				</LocalizationProvider>
-			</div>
-		);
+const CssTextField = styled(TextField, {
+	shouldForwardProp: (props) => props !== "focusColor",
+})((p) => ({
+	"& .MuiOutlinedInput-root": {
+		"&.Mui-focused fieldset": {
+			border: "1px solid",
+			borderColor: p.focusColor,
+		},
+	},
+}));
+
+export default function DatePickerMobile({ startDate, endDate, setStartDate, setEndDate }) {
+	const [value, setValue] = React.useState([startDate, endDate]);
+	const [onTime, setOnTime] = useState(today > endDate);
+	const today = new Date();
+
+	return (
+		<div>
+			<LocalizationProvider dateAdapter={AdapterDateFns}>
+				<Stack spacing={3} className="mobile-datepicker" sx={{ padding: 2 }}>
+					<MobileDateRangePicker
+						minDate={today}
+						startText="Start"
+						value={value}
+						onChange={(newValue) => {
+							setValue(newValue);
+							setStartDate(newValue[0]);
+							setEndDate(newValue[1]);
+						}}
+						renderInput={(startProps, endProps) => (
+							<React.Fragment>
+								<CssTextField
+									focusColor="rgba(0, 0, 0, 0.6)"
+									label="Start"
+									id={value.index}
+									size="small"
+									{...startProps}
+									InputLabelProps={{ style: { color: "rgba(0, 0, 0, 0.6)" } }}
+								/>
+								<Box sx={{ mx: 2 }}> to </Box>
+								<CssTextField
+									focusColor="rgba(0, 0, 0, 0.6)"
+									label="End"
+									id={value.index}
+									size="small"
+									color={onTime ? "primary" : "warning"}
+									{...endProps}
+									InputLabelProps={{ style: { color: "rgba(0, 0, 0, 0.6)" } }}
+								/>
+							</React.Fragment>
+						)}
+					/>
+				</Stack>
+			</LocalizationProvider>
+		</div>
+	);
 }
