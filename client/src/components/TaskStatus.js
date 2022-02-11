0d7fab19b02377d3360962e7f26bbb461669550d
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-02-11 12:02:00 +0000

    fix: remove console log

--- client/src/components/TaskStatus.js
+++ client/src/components/TaskStatus.js
@@ -16,7 +16,6 @@ export default function TaskStatus({ task, editTask }) {
 	const handleChangeTaskStatus = (task_id, description, status) => {
 		editTask(task_id, description, status)
 			.then((res) => {
-				console.log(res);
 				setChecked(res.data.status === "completed" ? true : false);
 			})
 			.catch((e) => console.log(e));
